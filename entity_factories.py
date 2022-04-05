774a7804074e5766e605e12b183f0b96cf009f6d
Nathaniel Berube <Rexar14@gmail.com>
2022-04-04 17:36:08 -0700

    Part 6, refactoring and doing/taking damage

--- entity_factories.py
+++ entity_factories.py
@@ -1,6 +1,28 @@
+from components.ai import HostileEnemy
+from components.fighter import Fighter
+from entity import Actor
 from entity import Entity
 
-player = Entity(char="@", color=(126, 222, 100), name="Player", blocks_movement=True)
+player = Actor(
+    char="@",
+    color=(126, 222, 100),
+    name="Player",
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=30, defense=2, power=5),
+)
 
-orc = Entity(char="o", color=(63, 127, 63), name="Orc", blocks_movement=True)
-troll = Entity(char="T", color=(62, 222, 187), name="Troll", blocks_movement=True)
+orc = Actor(
+    char="o",
+    color=(63, 127, 63),
+    name="Orc",
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=10, defense=0, power=3),
+)
+
+troll = Actor(
+    char="T",
+    color=(62, 222, 187),
+    name="Troll",
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=16, defense=1, power=4),
+)
